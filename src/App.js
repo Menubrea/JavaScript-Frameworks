e1de2e5e57f99fc020f4304ce686b30fd04e46be
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-27 09:23:13 +0100

    Updated paths and routing for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/UI/Layout';
+import { HomePage } from './components/Home';
+import { ProductPage } from './components/Product/index';
+import { GetApi } from './API/getData';
+
+const url = 'https://api.noroff.dev/api/v1/online-shop/';
 
 function App() {
+  const { data, isLoading, isError } = GetApi(url);
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <HomePage data={data} isLoading={isLoading} isError={isError} />
+            }
+          />
+          <Route
+            path="/:id"
+            element={
+              <ProductPage
+                data={data}
+                isLoading={isLoading}
+                isError={isError}
+              />
+            }
+          />
+        </Route>
       </Routes>
     </div>
   );
