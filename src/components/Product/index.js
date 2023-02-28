d878d4204079c3412ebcdff716e735357f8ec340
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-28 21:05:21 +0100

    Updated properties in product component

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,11 +1,16 @@
 import { CustomLink } from '../utilities';
 import { Product } from './Product';
 
-export function ProductPage() {
+export function ProductPage({ data, isLoading, isError, dispatch }) {
   return (
     <>
-      <Product />
-      <CustomLink to="/" name="Back to Home" />
+      <Product
+        data={data}
+        isError={isError}
+        isLoading={isLoading}
+        dispatch={dispatch}
+      />
+      <CustomLink to="/" name="Back Home" />
     </>
   );
 }
