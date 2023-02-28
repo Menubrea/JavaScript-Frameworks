dd93049b5a81ea2b01645ded29802d8c91f10895
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-28 20:59:54 +0100

    Updated styling to header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,29 +1,40 @@
 import { Nav } from './Nav';
 import styled from 'styled-components';
 import * as colors from '../../styles/Colors';
+import { Link } from 'react-router-dom';
 
 const BaseHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 1em;
+  height: 3.5em;
+  border-bottom: 1px dashed ${colors.primary};
 
-  div {
-    background-color: ${colors.primary};
-    height: fit-content;
-    width: fit-content;
-    padding: 0.2em 0.5em;
-    border-radius: 5px;
-    color: ${colors.light};
-    font-size: 20px;
+  a {
+    text-decoration: none;
   }
 `;
 
-export function Header() {
+const Logo = styled.div`
+  background-color: ${colors.accent};
+  height: fit-content;
+  width: fit-content;
+  padding: 0.2em 0.5em;
+  border-radius: 5px;
+  color: ${colors.light};
+  font-size: 18px;
+`;
+
+export function Header({ state, dispatch }) {
   return (
     <BaseHeader>
-      <div>&#8476;</div>
-      <Nav />
+      <Link to="/">
+        <Logo>
+          <span>&#8476;</span>emote
+        </Logo>
+      </Link>
+      <Nav state={state} dispatch={dispatch} />
     </BaseHeader>
   );
 }
